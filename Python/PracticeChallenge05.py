ea920bd87132b823603191a3d6d49d91b57baf69
4rmena <4rmena@gmail.com>
2026-09-12 22:51:37 +0800

    Day 11: Mini Sets Projects

---
+++ Python/PracticeChallenge05.py
@@ -0,0 +1,88 @@
+# UNIQUE SENSOR IDs
+
+pass1 = ["S1", "S2", "S3", "S2", "S4"]
+pass2 = ["S3", "S5", "S1"]
+pass3 = ["S6", "S2", "S1", "S1"]
+
+set1 = set(pass1)
+set2 = set(pass2)
+set3 = set(pass3)
+
+sets = set.union(set1 , set2, set3)
+
+print(set1)
+print(sets)
+
+print()
+
+# DUPLICATE TELEMETRY DETECTION
+readings = [101, 102, 102, 103, 104, 104, 104, 105]
+
+def findDuplicates(data):
+    setReadings = set(data)
+    common = set.intersection(setReadings)
+    total = len(common)
+    return common, total
+
+common, total = findDuplicates(readings)
+print(f"Values that appeared more than once: {total}")
+print(f"Values: {common}")
+
+print()
+
+# REDUNDANT SYSTEMS CHECK
+arrayA_active = {"propulsion", "comms", "life_support", "navigation"}
+arrayB_active = {"comms", "navigation", "thermal", "power"}
+
+active = set.intersection(arrayA_active, arrayB_active)
+array = set.union(arrayB_active, arrayA_active)
+diff = set.difference(arrayA_active, arrayB_active)
+unique = set.symmetric_difference(arrayA_active, arrayB_active)
+
+print(f"Active in both arrays: {active}")
+print(f"Seen in both arrays: {array}")
+print(f"Reportings : {diff}")
+print(f"Unique reporting(s): {unique}")
+
+print()
+
+# CREW ROSTER VALIDATION
+flight_ops_roster = {"Chen", "Alvarez", "Osei", "Kowalski"}
+medical_roster = {"Alvarez", "Kowalski", "Reyes", "Chen"}
+
+def roster_mismatch(setA, setB):
+    rosterA = set(setA)
+    rosterB = set(setB)
+    finalRoster = set.union(rosterA, rosterB)
+
+    if rosterA != rosterB:
+        mismatch = set.symmetric_difference(rosterA, rosterB)
+    else:
+        print("Match!")
+
+    return mismatch, finalRoster
+
+mismatch, finalRoster = roster_mismatch(flight_ops_roster, medical_roster)
+
+print(f"Persons missing from each roster: {mismatch}")
+print(f"Approved Final Roster: {finalRoster}")
+
+print()
+
+# MISSION CONTROL DASHBOARD (CAPSTONE)
+def alertSummary(log):
+
+    uniqueCodeSets = set(log)
+
+    freq = {}
+
+    for count in log:
+        freq[count] = freq.get(count, 0) + 1
+        
+    return uniqueCodeSets, freq
+
+alert_log = [204, 204, 501, 302, 501, 501, 204, 610]
+
+setLogs = alertSummary(alert_log)
+
+print(setLogs)
