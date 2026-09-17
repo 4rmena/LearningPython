23e7a4cc1757c7bb9c55a540d76da35d92799253
4rmena <4rmena@gmail.com>
2026-09-17 23:30:25 +0800

    Day 16: station summaries (indexes)

--- Python/capstone.py
+++ Python/capstone.py
@@ -18,33 +18,36 @@ def missionReport(*stations):
             bestCount = currentCount
             mostFreqCode = code
 
-            
-    return len(totalFaults), uniqueCodes, commonCodes, mostFreqCode, stationSum()
+    index = 1
+    
+    while index <= len(stations):
+
+        for faultCount in stations:
+            faultCount = len(stations)
+
+        print(index)
+
+        index += 1
+
+    print(faultCount)
+
+
+
+    return len(totalFaults), uniqueCodes, commonCodes, mostFreqCode
 
 #    stationSum = [tuple(stations[0]), tuple(stations[1]), tuple(stations[2])]
-"""
-    def stationSum():
-        stationIndex = 0
-        index = len(stations)
-
-        while index < stationIndex:
-            stationIndex += 1
-
-        return stationIndex
-"""
 #    for stationIndex, faultCount, worstCode in stations:
 #        pass
-
 #        stationSum = stationIndex, faultCount, worstCode
 
 stationA = [204, 204, 501, 302, 501]
 stationB = [204, 610, 501, 204]
 stationC = [302, 204, 501, 204, 610, 610]
 
-totalFaults, uniqueCodes, commonCodes, mostFreqCode, stationSum = missionReport( stationA, stationB, stationC)
+totalFaults, uniqueCodes, commonCodes, mostFreqCode = missionReport( stationA, stationB, stationC)
 
 print(totalFaults)
 print(uniqueCodes)
 print(commonCodes)
 print(mostFreqCode)
-print(stationSum)
+# print(stationSum)
